64b060cea90a944e89802ff635f5153b9df73e93
yura <perminov1901@bk.ru>
2024-11-11 02:51:55 +0300

    added image writer

--- src/main/java/org/ComplexRenderEngine/RenderEngine/ImageWriter.java
+++ src/main/java/org/ComplexRenderEngine/RenderEngine/ImageWriter.java
@@ -1,4 +1,17 @@
 package org.ComplexRenderEngine.RenderEngine;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class ImageWriter {
+    public void saveBufferedImage(BufferedImage image) {
+        try {
+            File outputFile = new File("output_image.png");
+            ImageIO.write(image, "PNG", outputFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
